6c3e4ded4374d70476e491075cadd2b01f722975
Dan C <crayne@x220dc.localdomain>
2017-08-06 11:13:50 -0700

    add script to init database to testing environment

--- test/initializeTestEnvironment.js
+++ test/initializeTestEnvironment.js
@@ -1,3 +1,66 @@
-require('./dropAllTables.js');
-require('../db/createTables.js');
-require('./insertTestData.js');
+var Promise   = require('promise');
+var sqlite    = require('sqlite');
+var path      = require('path');
+var fs        = require('fs');
+var sqlite3   = require('sqlite3').verbose();
+
+var dbDir     = path.resolve(process.cwd() + '/../') + '/db/';
+var testDir   = path.resolve(process.cwd() + '/../') + '/test/';
+
+var db        = new sqlite3.Database(dbDir + 'giftex.db');
+
+
+/* Drop all tables */
+var promise = new Promise(function(resolve, reject) {
+  fs.readFile(testDir + 'dropAllTables.sql', function (err, data) {
+    if (err) reject(err);
+    else resolve(data);
+  });
+
+}).then(function(dropAllTablesSql) {
+  return new Promise(function(resolve, reject) {
+    db.exec(dropAllTablesSql.toString(), function(err) {
+      if (err) reject(err);
+      else resolve(db);
+    }); 
+  })
+
+
+/* Create all tables */
+}).then(function() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(dbDir + 'createTables.sql', function (err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+
+}).then(function(createTablesSql) {
+  return new Promise(function(resolve, reject) {
+    db.exec(createTablesSql.toString(), function(err) {
+      if (err) reject(err);
+      else resolve(db);
+    }); 
+  })
+
+
+/* Insert test data */
+}).then(function() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(testDir + 'insertTestData.sql', function (err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+
+}).then(function(insertTestDataSql) {
+  return new Promise(function(resolve, reject) {
+    db.exec(insertTestDataSql.toString(), function(err) {
+      if (err) reject(err);
+      else resolve(db);
+    }); 
+  })
+
+}).catch(function(err) {
+  console.log("ERROR!" + err);
+});
